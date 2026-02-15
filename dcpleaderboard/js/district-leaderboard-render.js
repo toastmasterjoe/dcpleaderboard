412fb55967d39a6992b08a61abf1322b13f51f89
Joseph Galea <joseph.galea@bluetoolcase.com>
2026-02-15 13:10:53 +0100

    resetting district leaderboard to 200 items

--- dcpleaderboard/js/district-leaderboard-render.js
+++ dcpleaderboard/js/district-leaderboard-render.js
@@ -163,7 +163,7 @@ function init_document($) {
     serverSide: false,
     responsive: true,
     ordering: false,
-    pageLength: 20,
+    pageLength: 200,
     lengthMenu: [10, 20, 50, 100, 200],
     serverMethod: "get",
     dom: '<"top-controls"lf>rt<"bottom-controls"ip>',
